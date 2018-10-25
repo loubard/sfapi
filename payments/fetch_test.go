9d3d6a843815778d151fd888e8f6d77b4fc5351a
Kevin 42 <kevin@42boxes.com>
2018-10-25 21:58:28 +0100

    Update test for Fetch and add test for not found

--- payments/fetch_test.go
+++ payments/fetch_test.go
@@ -2,30 +2,45 @@ package payments
 
 import (
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/loubard/sfapi/sql"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFetch(t *testing.T) {
 	db, err := gorm.Open("sqlite3", ":memory:")
 	assert.NoError(t, err)
-	req := httptest.NewRequest("GET", "http://example.com/v1/payments/id", nil)
+	sql.Seed(db)
+
+	req := httptest.NewRequest("GET", "http://example.com/v1/payments/4ee3a8d8-ca7b-4290-a52c-dd5b6165ec43", nil)
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/payments/{id}", Fetch(db))
+	router.ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Contains(t, string(body), "{\"data\":")
+}
+
+func TestFetchNotFound(t *testing.T) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	assert.NoError(t, err)
+	req := httptest.NewRequest("GET", "http://example.com/v1/payments/not-found", nil)
 	w := httptest.NewRecorder()
 
 	Fetch(db)(w, req)
 
 	resp := w.Result()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Can't read body from get detail response")
-	}
-
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	assert.Contains(t, string(body), "{\"data\":")
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
 }
